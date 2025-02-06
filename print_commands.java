c06c28b250008b5cbf0682eedb862cf336fd291e
Kaan Can Köse <kaancankose1923@gmail.com>
2025-02-06 13:20:57 +0300

    Add files via upload

---
+++ print_commands.java
@@ -0,0 +1,27 @@
+public class print_commands {
+
+        public static void main(String[] args) {
+            System.out.println("Hello, World!"); // Prints text and moves to the next line
+            System.out.println("This is another line.");
+
+            System.out.print("Hello, ");
+            System.out.print("World!");
+            // The output will be: Hello, World!
+
+
+            String name = "Pascal_Nouma";
+            int age = 31;
+            System.out.printf("Name: %s, Age: %d%n", name, age);
+            // %s is for strings, %d is for integers, and %n adds a new line.
+                }
+
+
+
+
+
+
+        }
+
+
+
+
